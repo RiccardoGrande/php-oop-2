945572fb6a6d48fec24c1e204f9c21a093c6f6da
RiccardoGrande <grandericcardo1997@gmail.com>
2023-05-17 16:29:15 +0200

    add staart

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+class Computer
+{
+    public $sistema;
+    public $batteria;
+    public $rete;
+
+}
+
+class Desktop extends Computer
+{
+
+}
+
+class Portatile extends Computer
+{
+
+}
